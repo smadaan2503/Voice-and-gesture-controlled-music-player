85866b4891dedc4928a833151b8f0d3ac5c9ec5d
smadaan2503 <s.madaan25@gmail.com>
2019-09-25 01:00:44 +0530

    Added database

--- musicplayer/admin.py
+++ musicplayer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SongsData
 
+admin.site.register(SongsData)
 # Register your models here.
--- musicplayer/models.py
+++ musicplayer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SongsData(models.Model):
+    Name = models.CharField(max_length = 100)
+    Singer = models.CharField(max_length = 100)
+    Lyrics = models.TextField()
+    Audio = models.FileField(upload_to = 'musics/')
+    Thumbnail = models.ImageField(upload_to = 'thumbnails/')
+    Category = models.CharField(max_length = 100)
